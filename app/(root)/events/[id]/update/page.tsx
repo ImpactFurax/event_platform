6af70b97d82989f8b32fe4623097f7f22511baf1
ImpactFurax <bortjean@yahoo.fr>
2024-01-06 17:36:33 +0100

    implement stripe

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,10 +1,18 @@
 import EventForm from "@/components/shared/EventForm"
+import { getEventById } from "@/lib/actions/event.actions";
 import { auth } from "@clerk/nextjs"
 
-const UpdateEvent = () => {
+type UpdateEventProps = {
+  params: {
+    id: string
+  }
+}
+
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
   const { sessionClaims } = auth();
 
   const userId = sessionClaims?.userId as string;
+  const event = await getEventById(id)
 
   return (
     <>
@@ -15,6 +23,8 @@ const UpdateEvent = () => {
       <div className="wrapper my-8">
         <EventForm
           userId={userId}
+          event={event}
+          eventId={event._id}
           type="Update"
         />
       </div>
